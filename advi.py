ecc9e637bb38a80089389a1ae9b9023bc492b426
QuentinJGMace <quentinjg.mace@gmail.com>
2024-03-21 00:09:45 +0100

    Refactoring, new approach, advi2 is the good one

--- advi.py
+++ advi.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
+from torch import Tensor
 from torch.distributions import Normal, MultivariateNormal, Gamma
 import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 class ModelParam(nn.Module):
     def __init__(self, dim, mode="meanfield"):
@@ -17,6 +20,7 @@ class ModelParam(nn.Module):
             self.mean = torch.zeros(dim)
             self.log_std = torch.zeros(dim)
 
+            #concatenates the mean and the log_std into a 1D tensor
             self.vparams = torch.stack([self.mean, self.log_std])
         # elif self.mode == "fullrank":
         #     self.mean = torch.zeros(dim)
@@ -41,13 +45,11 @@ class ModelParam(nn.Module):
         return self.dist().log_prob(x).sum()
         # elif self.mode == "fullrank":
         #     return MultivariateNormal(self.mean, self.L @ self.L.t()).log_prob(x).sum()
-        
 
 
 class ADVI:
-    def __init__(self, model, inv_T, num_samples=1, lr=0.01, max_iter=1000, mode="fullrank"):
+    def __init__(self, model, param_keys, param_dims, key_pos, num_samples=1, lr=0.01, max_iter=1000, mode="fullrank"):
         self.model = model
-        self.inv_T = inv_T
         self.delta_elbo_threshold = 1e-6
         self.num_samples = num_samples
         self.max_iter = max_iter
@@ -55,20 +57,26 @@ class ADVI:
         self.mode = mode
         self.model_params = {}
 
+        self.param_keys = param_keys
+        self.param_dims = param_dims
+        self.key_pos = key_pos
+
         if mode not in ["meanfield", "fullrank"]:
             raise ValueError("mode should be either 'meanfield' or 'fullrank'")
         
         if self.mode == "meanfield":
-            self.model.init_meanfield()
+            self.init_meanfield()
         elif self.mode == "fullrank":
             raise NotImplementedError("Fullrank not implemented yet")
-            self.model.init_fullrank()
+            self.init_fullrank()
+
+        # print([self.model_params[key] for key in self.model_params.keys()])
 
     def init_meanfield(self):
-        self.model_params = {'mu' : ModelParam(self.model.d, mode="meanfield"),
-                             'log_sigma' : ModelParam(self.model.d, mode="meanfield")
-                            }
-        
+        self.model_params = {}
+        for i, key in enumerate(self.param_keys):
+            self.model_params[key] = ModelParam(self.param_dims[key], mode="meanfield")
+                
     def log_prior(self, real_params, params):
         """
         The log prior of the parameters
@@ -76,42 +84,49 @@ class ADVI:
         params: same as real_params but in their orignal space"""
         if self.mode == "meanfield":
             log_mu = Normal(0, 1).log_prob(params['mu']).sum()
-            log_sigma = Gamma(1, 1).log_prob(params['sigma']).sum()
+            # Counter intuitive, params represent the exponentiated log sigma
+            # (to keep the same keys with real_params)
+            log_sigma = Gamma(1, 1).log_prob(params['log_sigma']).sum()
 
-    def det_J(self, real_params, params):
+    # DEPEND DES PARAMETRES DU MODELE
+    def det_J(self, real_params, key_pos:list):
         """
         log(|det(Jacobian_T-1)|) of the transformation from the real space to the original space"""
-        if self.mode == "meanfield":
-            return params['log_sigma'].sum()
-        
-    def entropy(self, real_params, params): #Up to a constant since we only need the gradient
+        sum = Tensor([0.])
+        sum.requires_grad = True
+        for key in self.key_pos:
+            sum += real_params[key].sum()
+        return sum
+    
+    def entropy(self, model_params): #Up to a constant since we only need the gradient
         """The entropy of the model"""
         if self.mode == "meanfield":
-            return params['log_sigma'].sum()
+            # return real_params['log_sigma'].exp().sum()
+            sum = Tensor([0.])
+            sum.requires_grad = True
+            for key in model_params.keys():
+                sum += model_params[key].log_std.sum()
         elif self.mode == "fullrank":
             raise NotImplementedError("Fullrank not implemented yet")
+        return sum
         
     def elbo(self, data):
         """The ELBO of the model"""
         real_params = {}
+        # print("ELBO !")
         for key in self.model_params:
-            real_params[key] = self.model_params[key].rsample(1)
-        
-        if self.mode == "meanfield":
-            params = {'mu': real_params['mu'],
-                      'log_sigma': real_params['log_sigma'].exp()}
-        elif self.mode == "fullrank":
-            raise NotImplementedError("Fullrank not implemented yet")
-            params = {'mu': real_params['mu'],
-                      'L': real_params['L']}
+            real_params[key] = self.model_params[key].rsample([1])
             
-        log_prior = self.log_prior(real_params, params)
-        log_q = self.log_q(real_params, params)
-        log_likelihood = self.model.log_prob(data, params)
-        entropy = self.entropy(real_params, params)
-        det_J = self.det_J(real_params, params)
-
+        # log_prior = self.log_prior(real_params, params)
+        log_q = self.log_q(real_params, self.model_params)
+        log_likelihood = self.model.log_prob(data, real_params)
+        entropy = self.entropy(self.model_params)
+        det_J = self.det_J(real_params, key_pos=self.key_pos)
+        
         elbo = log_likelihood + det_J + entropy
+        elbo.requires_grad = True
+        # with torch.no_grad():
+        #     print("ELBO: ", elbo, "\nlog_prior: ", log_prior, "\nlog_q: ", log_q, "\nlog_likelihood: ", log_likelihood, "\nentropy: ", entropy, "\ndet_J: ", det_J)
         return elbo
         
         
@@ -122,23 +137,21 @@ class ADVI:
         return out
         
     def init_fullrank(self):
-        self.model_params = {'mu' : ModelParam(self.model.d, mode="meanfield"),
-                             'L': ModelParam(self.model.d*(self.model.d + 1)/2, mode="meanfield")
-                            }
+        raise NotImplementedError("Fullrank not implemented yet")
     
-    def fit(self, X: torch.Tensor):
+    def fit(self, X: torch.Tensor, plotting:bool=False):
         """Fits the parameters using ADVI onto the given data X
         
         Parameters:
         X (torch.Tensor): The data to fit the parameters on"""
         self.model.train() # Activates the gradient computation
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr) # Not used in the paper, we could find an other one
+        optimizer = torch.optim.Adam([self.model_params[key].vparams for key in self.model_params.keys()], lr=self.lr) # Not used in the paper, we could find an other one
         if self.mode == "meanfield":
-            self.fit_meanfield(X, optimizer)
+            self.fit_meanfield(X, optimizer, plotting)
         elif self.mode == "fullrank":
-            self.fit_fullrank(X, optimizer)
+            self.fit_fullrank(X, optimizer, plotting)
 
-    def fit_meanfield(self, X: torch.Tensor, optimizer: torch.optim.Optimizer):
+    def fit_meanfield(self, X: torch.Tensor, optimizer: torch.optim.Optimizer, plotting:bool=False):
         """Fits the parameters using ADVI with meanfield approximation onto the given data X
         
         Parameters:
@@ -147,14 +160,20 @@ class ADVI:
 
         elbo, delta_elbo = 0, np.inf
         i = 0
+        bar = tqdm(total=self.max_iter)
+        elbo_list = []
         while i < self.max_iter and delta_elbo > self.delta_elbo_threshold:
             optimizer.zero_grad()
-            z = torch.randn(self.num_samples, self.model.num_params)
-            elbo = self.model.elbo(X, z, self.model.params)
+            elbo = self.elbo(X)
             loss = -elbo #Optimisers are for minimisation
             loss.backward()
             optimizer.step()
             i += 1
+            elbo_list.append(elbo.item())
+            bar.update(1)
+        if plotting:
+            plt.plot(elbo_list)
+            plt.show()
     
-    def fit_fullrank(self, X: torch.Tensor, optimiser: torch.optim.Optimizer):
+    def fit_fullrank(self, X: torch.Tensor, optimiser: torch.optim.Optimizer, plotting:bool=False):
         raise NotImplementedError("Fullrank not implemented yet")
--- ppca.py
+++ ppca.py
@@ -1,6 +1,7 @@
 import torch
+from torch import Tensor
 import torch.nn as nn
-from torch.distributions import Normal, Gamma
+from torch.distributions import Normal, Gamma, MultivariateNormal
 import numpy as np
 
 class PPCA_with_ARD_model(nn.Module):
@@ -18,26 +19,68 @@ class PPCA_with_ARD_model(nn.Module):
         self.d = D # data dimension
 
         # Simple PPCA without ARD but with unconstrained noise variance
-        self.log_sigma = nn.Parameter(torch.zeros(self.d))
-        self.W = nn.Parameter(torch.zeros(self.d, self.K))
-        self.mu = nn.Parameter(torch.zeros(self.d))
+        # self.log_sigma = nn.Parameter(torch.zeros(self.d))
+        # self.W = nn.Parameter(torch.zeros(self.d, self.K))
+        # self.mu = nn.Parameter(torch.zeros(self.d))
+        self.eps = 1e-6
 
         # ARD
         # Multiplies each column of W by the corresponding element of alpha (to nullify some of them)
-        self.log_alpha = nn.Parameter(torch.zeros(self.K))
+        # self.log_alpha = nn.Parameter(torch.zeros(self.K))
 
-    def dist(self):
+        self.num_parameters = self.d * self.K + self.d + self.d + self.K # W, mu, log_sigma, log_alpha
+
+        self.named_params = ["W", "mu", "log_sigma", "log_alpha"]
+        self.dim_parameters = {"W": self.d*self.K, "mu": self.d, "log_sigma": self.d, "log_alpha": self.K}
+        self.key_pos = ["log_sigma", "log_alpha"]
+
+    def dist(self, W, mu, sigma, alpha):
         """Returns the distribution of the model"""
         # Multiply the column i of W by alpha_i
-        W_multiplied = self.W * torch.exp(self.log_alpha)
-        var_noise = self.log_sigma.exp()*torch.eye(self.d)
-        mean = self.mu
-        return Normal(mean, torch.mm(W_multiplied, W_multiplied.t()) + var_noise)
+        W_multiplied = W.view(self.d, self.K) * alpha
+        var_noise = sigma*torch.eye(self.d) + self.eps # Throws an error if the noise variance is 0
+        mean = mu
+        return MultivariateNormal(mean, torch.mm(W_multiplied, W_multiplied.t()) + var_noise)
     
     def rsample(self, n):
         """Samples n points from the distribution"""
         return self.dist().rsample([n])
     
-    def log_prob(self, x):
+    def theta_from_zeta(self, zeta):
+        """Returns the model parameters from the tensor zeta"""
+        W = zeta[:self.d*self.K]
+        mu = zeta[self.d*self.K:self.d*self.K+self.d]
+        sigma = torch.exp(zeta[self.d*self.K+self.d:self.d*self.K+2*self.d])
+        alpha = torch.exp(zeta[self.d*self.K+2*self.d:])
+
+        # concatenates the paremeters and returns them as one single tensor
+        return torch.cat([W, mu, sigma, alpha])
+    
+    def grad_inv_T(self, zeta):
+        """Returns the gradient of the inverse transformation"""
+        with torch.no_grad():
+            W = torch.ones(self.d*self.K)
+            mu = torch.ones(self.d)
+            sigma = torch.exp(zeta[self.d*self.K+self.d:self.d*self.K+2*self.d])
+            alpha = torch.exp(zeta[self.d*self.K+2*self.d:])
+            
+            return torch.cat([W, mu, sigma, alpha])
+        
+    def log_det(self, zeta):
+        """Returns the log determinant of the jacobian of the inverse transformation"""
+        return torch.sum(zeta[self.d*self.K+self.d:self.d*self.K+2*self.d]) + torch.sum(zeta[self.d*self.K+2*self.d:])
+    
+    def log_prob(self, x, theta, full_data_size):
         """Returns the log probability of x under the distribution of the model"""
-        return self.dist().log_prob(x)
+        # gets the model parameters from the tensor params
+        W = theta[:self.d*self.K].view(self.d, self.K)
+        mu = theta[self.d*self.K:self.d*self.K+self.d]
+        sigma = theta[self.d*self.K+self.d:self.d*self.K+2*self.d]
+        alpha = theta[self.d*self.K+2*self.d:]
+
+        # sums the log probabilities of each point
+        sum = 0.
+        for i in range(x.shape[0]):
+            sum += self.dist(W, mu, sigma, alpha).log_prob(x[i]).sum()
+        return sum*full_data_size/x.shape[0]
+        
