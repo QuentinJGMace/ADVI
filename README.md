7c3df4b3c24cefd87b90b3bfc9d3031cb921ab87
QuentinJGMace <quentinjg.mace@gmail.com>
2024-03-21 17:58:36 +0100

    maj readme + doc

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# ADVI
+# Structure du repo
+
+- advi.py : ne marche pas, outdated
+- advi2.py : version qui marche
+    - Contient 2 classes : ModelParam2, ADVI2
+    - ModelParam2 : sert à gérer les paramètres qu'apprend l'advi (peut les sampler, retrouner leur distribution et contient leur moyenne + std)
+    - ADVI2 : algo en lui même, méthode fit qui permet de fit un modèle à des données
+- ppca.py : Contient les modèles pour la ppca (**PPCA_model, PPCA_with_ARD_model**), tous les paramètres et les calculs importants pour ADVI (log_vraissemblance en particulier)
+- test.ipynb, test.py : fichiers de test avec un petit jeu de donnée en 2D pour voir si ça fonctionne.
+- data_exploration.ipynb : Fichier ou j'ai lu et mis en forme les données sur les taxis
+
+# Ajout d'un nouveau modèle
+Le modèle doit avoir les même méthodes que dans le fichier ppca.py. C'est à dire :
+## Attributs
+- self.num_parameters : la dimension totale des paramètres (i.e mean + sigma en 2 dimension --> num_parameters = 4)
+- self.named_params (en vrai pas besoin) : des clés pour chacun des paramètres
+- self.dim_parameters : dictionnaire {clé de paramètre : dimension du paramètre}
+- self.key_pos : les clés des paramètres qui sont passés au log (typiquement std)
+
+## Méthodes
+- dist(\*\*params) : retourne la distribution du modèle selon les paramètres données
+- rsample(n, \*\*params) : sample n points selon self.dist(\*\*params)
+- theta_from_zeta(zeta) : retourne le vecteur de paramètre $\theta = T^{-1}(\zeta)$
+- grad_inv_T: OSEF, plus utilisée, c'est calculé par pytorch
+- log_det(zeta) : retourne $log(|det(J_{T^{-1}}(\zeta))|)$
+- log_prob(x, theta, full_data_size) : retourne la log vraissemblance des données selon le paramètre theta. Full data size est la taille totale du jeu de données si x ne représente qu'un sous ensemble.
--- ppca.py
+++ ppca.py
@@ -20,10 +20,12 @@ class PPCA_model(nn.Module):
 
         self.eps = 1e-6
 
+        # Variables used for ADVI to know the dimensionality of the optimization problem
         self.num_parameters = self.d * self.K + self.d + self.d # W, mu, log_sigma
 
         self.named_params = ["W", "mu", "log_sigma"]
         self.dim_parameters = {"W": self.d*self.K, "mu": self.d, "log_sigma": self.d}
+        # Key_pos is now ununsed but it keeps the model variables that are supposed to be positive
         self.key_pos = ["log_sigma"]
 
     def dist(self, W, mu, sigma):
@@ -33,6 +35,7 @@ class PPCA_model(nn.Module):
             breakpoint()
         var_noise = sigma*torch.eye(self.d) + self.eps # Throws an error if the noise variance is 0
         mean = mu
+        # Try / Except to understand errors
         try:
             return MultivariateNormal(mean, torch.mm(W_multiplied, W_multiplied.t()) + var_noise)
         except:
@@ -45,8 +48,8 @@ class PPCA_model(nn.Module):
         """Samples n points from the distribution"""
         return self.dist(W, mu, sigma).rsample([n])
     
-    def theta_from_zeta(self, zeta):
-        """Returns the model parameters from the tensor zeta"""
+    def theta_from_zeta(self, zeta:torch.Tensor):
+        """Returns the model parameters from the tensor zeta (applies T^-1)"""
         W = zeta[:self.d*self.K]
         mu = zeta[self.d*self.K:self.d*self.K+self.d]
         sigma = torch.exp(zeta[self.d*self.K+self.d:self.d*self.K+2*self.d])
@@ -56,8 +59,10 @@ class PPCA_model(nn.Module):
         # concatenates the paremeters and returns them as one single tensor
         return torch.cat([W, mu, sigma])
     
-    def grad_inv_T(self, zeta):
-        """Returns the gradient of the inverse transformation"""
+    def grad_inv_T(self, zeta:torch.Tensor):
+        """
+        DEPRECATED
+        Returns the gradient of the inverse transformation"""
         with torch.no_grad():
             W = torch.ones(self.d*self.K)
             mu = torch.ones(self.d)
@@ -65,20 +70,25 @@ class PPCA_model(nn.Module):
             
             return torch.cat([W, mu, sigma])
         
-    def log_det(self, zeta):
+    def log_det(self, zeta:torch.Tensor):
         """Returns the log determinant of the jacobian of the inverse transformation"""
         return torch.sum(zeta[self.d*self.K+self.d:self.d*self.K+2*self.d])
     
-    def log_prob(self, x, theta, full_data_size):
-        """Returns the log probability of x under the distribution of the model"""
+    def log_prob(self, x:torch.Tensor, theta:torch.Tensor, full_data_size:int):
+        """Returns the log probability of x under the distribution of the model
+        
+        Args:
+            x: torch.Tensor, data
+            theta: torch.Tensor, model parameters
+            full_data_size: int, size of the full data set (used for batch learning)"""
         # gets the model parameters from the tensor params
         W = theta[:self.d*self.K].view(self.d, self.K)
         mu = theta[self.d*self.K:self.d*self.K+self.d]
         sigma = theta[self.d*self.K+self.d:self.d*self.K+2*self.d]
 
-        assert(sigma > 0.).all() 
+        assert(sigma > 0.).all() # Debugging
 
-        # sums the log probabilities of each point
+        # Sums (and scales for batch learning) the log probabilities of each point
         sum = 0.
         for i in range(x.shape[0]):
             sum += self.dist(W, mu, sigma).log_prob(x[i]).sum()
@@ -99,15 +109,7 @@ class PPCA_with_ARD_model(nn.Module):
         self.K = K # latent dimension
         self.d = D # data dimension
 
-        # Simple PPCA without ARD but with unconstrained noise variance
-        # self.log_sigma = nn.Parameter(torch.zeros(self.d))
-        # self.W = nn.Parameter(torch.zeros(self.d, self.K))
-        # self.mu = nn.Parameter(torch.zeros(self.d))
         self.eps = 1e-6
-
-        # ARD
-        # Multiplies each column of W by the corresponding element of alpha (to nullify some of them)
-        # self.log_alpha = nn.Parameter(torch.zeros(self.K))
 
         self.num_parameters = self.d * self.K + self.d + self.d + self.K # W, mu, log_sigma, log_alpha
 
@@ -115,9 +117,9 @@ class PPCA_with_ARD_model(nn.Module):
         self.dim_parameters = {"W": self.d*self.K, "mu": self.d, "log_sigma": self.d, "log_alpha": self.K}
         self.key_pos = ["log_sigma", "log_alpha"]
 
-    def dist(self, W, mu, sigma, alpha):
+    def dist(self, W:torch.Tensor, mu:torch.Tensor, sigma:torch.Tensor, alpha:torch.Tensor):
         """Returns the distribution of the model"""
-        # Multiply the column i of W by alpha_i
+        # Multiply the column i of W by alpha_i (ARD)
         W_multiplied = W.view(self.d, self.K) * alpha
         var_noise = sigma*torch.eye(self.d) + self.eps # Throws an error if the noise variance is 0
         mean = mu
@@ -130,11 +132,11 @@ class PPCA_with_ARD_model(nn.Module):
             print("var", var_noise)
             raise Exception("Error in the distribution")
     
-    def rsample(self, n, W, mu, sigma, alpha):
+    def rsample(self, n:int, W:torch.Tensor, mu:torch.Tensor, sigma:torch.Tensor, alpha:torch.Tensor):
         """Samples n points from the distribution"""
         return self.dist(W, mu, sigma, alpha).rsample([n])
     
-    def theta_from_zeta(self, zeta):
+    def theta_from_zeta(self, zeta:torch.Tensor):
         """Returns the model parameters from the tensor zeta"""
         W = zeta[:self.d*self.K]
         mu = zeta[self.d*self.K:self.d*self.K+self.d]
@@ -147,7 +149,7 @@ class PPCA_with_ARD_model(nn.Module):
         # concatenates the paremeters and returns them as one single tensor
         return torch.cat([W, mu, sigma, alpha])
     
-    def grad_inv_T(self, zeta):
+    def grad_inv_T(self, zeta:torch.Tensor):
         """Returns the gradient of the inverse transformation"""
         with torch.no_grad():
             W = torch.ones(self.d*self.K)
@@ -157,11 +159,11 @@ class PPCA_with_ARD_model(nn.Module):
             
             return torch.cat([W, mu, sigma, alpha])
         
-    def log_det(self, zeta):
+    def log_det(self, zeta:torch.Tensor):
         """Returns the log determinant of the jacobian of the inverse transformation"""
         return torch.sum(zeta[self.d*self.K+self.d:self.d*self.K+2*self.d]) + torch.sum(zeta[self.d*self.K+2*self.d:])
     
-    def log_prob(self, x, theta, full_data_size):
+    def log_prob(self, x:torch.Tensor, theta:torch.Tensor, full_data_size:int):
         """Returns the log probability of x under the distribution of the model"""
         # gets the model parameters from the tensor params
         W = theta[:self.d*self.K].view(self.d, self.K)
